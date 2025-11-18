a8d04611b57ec9c01a5af64d5cb1a7b81f61abb3
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2025-11-18 16:05:37 +0400

    fix

--- app/(admin)/agents/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import AdminDashboardMain from "@/components/admin/AdminDashboardMain";
-
-export default function AdminAgentsPage() {
-  return <AdminDashboardMain />;
-}
