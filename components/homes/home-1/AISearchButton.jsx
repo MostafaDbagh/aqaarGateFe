0121bfdb931349358fae912bc07ea66472d406eb
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2025-12-15 00:58:16 +0400

    fix

--- components/homes/home-1/AISearchButton.jsx
+++ components/homes/home-1/AISearchButton.jsx
@@ -13,6 +13,7 @@ import styles from "./AISearchButton.module.css";
  */
 export default function AISearchButton({ onSearchResults }) {
   const t = useTranslations('hero');
+  const tPropertyList = useTranslations('propertyList');
   const locale = useLocale();
   const isRTL = locale === 'ar';
   const router = useRouter();
@@ -348,6 +349,18 @@ export default function AISearchButton({ onSearchResults }) {
                 </button>
               </div>
             </form>
+
+            {/* AI Parser Warning */}
+            <div className={`${styles.aiParserWarning} ${isRTL ? styles.rtl : styles.ltr}`}>
+              <div className={styles.warningIcon}>
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" fill="currentColor"/>
+                </svg>
+              </div>
+              <div className={styles.warningText}>
+                {tPropertyList('aiParserWarning')}
+              </div>
+            </div>
 
             {/* Example Queries */}
             {query.length === 0 && (
--- components/properties/AISearchBox.jsx
+++ components/properties/AISearchBox.jsx
@@ -196,8 +196,14 @@ export default function AISearchBox({ onResults, className = "" }) {
 
       {/* AI Parser Warning */}
       <div className={`${styles.aiParserWarning} ${isRTL ? styles.rtl : styles.ltr}`}>
-        <span>⚠️</span>
-        <span>{t('aiParserWarning')}</span>
+        <div className={styles.warningIcon}>
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" fill="currentColor"/>
+          </svg>
+        </div>
+        <div className={styles.warningText}>
+          {t('aiParserWarning')}
+        </div>
       </div>
 
       {/* Extracted Parameters */}
